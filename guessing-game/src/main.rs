e559203ac793867810b8cd62b4ea3a436b81dc27
jrasmusbm <JRasmusBm@gmail.com>
2021-06-14 09:40:30 +0200

    Add random target value
    **Why** is the change needed?
    
    The game is less fun if the answer is always the same.

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,8 +1,12 @@
+use rand::Rng;
+
 mod input;
 mod output;
 
 fn main() {
-    let target = 5;
+    let target = rand::thread_rng().gen_range(1..101);
+    println!("Welcome to the guessing game!");
+    println!("The secret number is between 1 and 100.");
 
     loop {
         let guess = input::get_guess_from_user();
